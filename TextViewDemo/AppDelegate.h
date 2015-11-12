11ed2a852acc34f6f7c8f8b6020ef74d8f85ae47
licong <licong@wanmeizhensuo.com>
2015-11-12 17:18:45 +0800

    Initial Commit

---
+++ TextViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextViewDemo
+//
+//  Created by licong on 11/12/15.
+//  Copyright © 2015 Sean Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
