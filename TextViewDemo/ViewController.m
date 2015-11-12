efa2606f73abc4f9328757c64a9c3c040bab5b4e
licong <licong@wanmeizhensuo.com>
2015-11-12 17:51:32 +0800

    save

--- TextViewDemo/ViewController.m
+++ TextViewDemo/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)buttonClicked:(id)sender {
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
