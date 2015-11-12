c407b7eafa7db909ad48efbe05cf0f02c4626c7b
licong <licong@wanmeizhensuo.com>
2015-11-12 17:50:31 +0800

    first commit

---
+++ TextViewDemo/ExampleVIewController.h
@@ -0,0 +1,13 @@
+//
+//  ExampleVIewController.h
+//  TextViewDemo
+//
+//  Created by licong on 11/12/15.
+//  Copyright © 2015 Sean Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExampleVIewController : UIViewController
+
+@end
---
+++ TextViewDemo/ExampleVIewController.m
@@ -0,0 +1,37 @@
+//
+//  ExampleVIewController.m
+//  TextViewDemo
+//
+//  Created by licong on 11/12/15.
+//  Copyright © 2015 Sean Lee. All rights reserved.
+//
+
+#import "ExampleVIewController.h"
+
+@interface ExampleVIewController ()
+
+@end
+
+@implementation ExampleVIewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
